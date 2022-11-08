6207145a2a851b4624503803176a11021a00dc5e
Liczkowski <101157512+Liczkowski@users.noreply.github.com>
2022-11-07 22:22:02 -0300

    Add files via upload

---
+++ Nova pasta/a=10.1.py
@@ -0,0 +1,33 @@
+def raiz_quadrada(a):
+    return math.sqrt(a)
+
+
+def potencia (a,b):
+    return a**b
+   
+
+
+def somar (a,b):
+    return a+b
+
+    def subtrair(a,b):
+            return a-b
+
+            def mult (a,b):
+             a*b
+
+    def div(a,b):
+            return a/b
+           
+           
+           
+            print (mult (10,20))
+            print(div (10,20))
+            print(somar(10,20))
+            print(subtrair(30,20))
+            print(potencia(10,20))
+            print (raiz_quadrada(9))
+
+a= float (input('digite o valor a '))
+b= float (input('digite o valor b '))
+print (a+b)
