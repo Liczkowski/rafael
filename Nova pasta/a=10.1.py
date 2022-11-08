c4135fdf36291efbc706458ad50b92a9cacabdfc
Liczkowski <101157512+Liczkowski@users.noreply.github.com>
2022-11-07 22:23:31 -0300

    Add files via upload

--- Nova pasta/a=10.1.py
+++ Nova pasta/a=10.1.py
@@ -1,33 +1,46 @@
-def raiz_quadrada(a):
-    return math.sqrt(a)
+def calcula_delta(a,b,c):
+return (b**2) -4*a*c
 
 
-def potencia (a,b):
-    return a**b
+a = float (input("digita a"))
+b= float (input('digite o valor a '))
+c= float(input('digite o valor a '))
+
+delta= calcula_delta(a,b,c)
+print(delta)
+
+
+# def raiz_quadrada(a):
+#     return math.sqrt(a)
+
+
+# def potencia (a,b):
+#     return a**b
    
 
 
-def somar (a,b):
-    return a+b
+# def somar (a,b):
+#     return a+b
 
-    def subtrair(a,b):
-            return a-b
+#     def subtrair(a,b):
+#             return a-b
 
-            def mult (a,b):
-             a*b
+#             def mult (a,b):
+#              a*b
 
-    def div(a,b):
-            return a/b
+#     def div(a,b):
+#             return a/b
            
            
            
-            print (mult (10,20))
-            print(div (10,20))
-            print(somar(10,20))
-            print(subtrair(30,20))
-            print(potencia(10,20))
-            print (raiz_quadrada(9))
+#             print (mult (10,20))
+#             print(div (10,20))
+#             print(somar(10,20))
+#             print(subtrair(30,20))
+#             print(potencia(10,20))
+#             print (raiz_quadrada(9))
 
-a= float (input('digite o valor a '))
-b= float (input('digite o valor b '))
-print (a+b)
+# a= float (input('digite o valor a '))
+# b= float (input('digite o valor b '))
+# print (a+b)
+
